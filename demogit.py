cbbc66c2acb8c6c9e7b4a641838245302b732d6f
Raghavendra S Prabhu <raghavendra.prabhu@marketmedium.com>
2019-09-10 15:57:26 +0530

    This is my second commit

--- demogit.py
+++ demogit.py
@@ -1,3 +1,14 @@
-print("raghavendra ")
-sdjfhjksahfasjfjsaf
+students=0
+teachers=0
+for i in range(int(input())):
+    t = input()
+    temp1 = t.find("@student.college.edu")
+    temp2 = t.find("@professor.college.edu")
+    if(temp1!=-1):
+        students=students+1
+    elif(temp2!=-1):
+        teachers=teachers+1
+    else:
+        pass
 
+print(students,teachers)
